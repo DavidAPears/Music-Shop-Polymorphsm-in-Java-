ff1494c5ce63265baed83c79b3a9a85b1dd02fe3
David A Pears <davidapears@gmail.com>
2018-07-09 09:52:47 +0100

    Shop Tests all working

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -26,6 +26,9 @@ public class ShopTest {
     public void before() {
         drumsticks = new Drumsticks("Accessories.Drumsticks", 1.99, 4.99);
         drumskins = new Drumskins("Accessories.Drumskins", 1.99, 9.99);
+        guitar = new Guitar(99.00, 499.00, "Mahogany", "Cherry Sunburst", "Guitar", 6, "Gibson");
+        piano = new Piano(250.00, 999.00, "Ebony", "Black", "Piano", 88, "Yamaha");
+        saxophone = new Saxophone( 59.00, 179.99, "Brass", "Gold", "Saxophone", 23, "Hanson");
         stock = new ArrayList<>();
         stock.add(guitar);
         stock.add(piano);
@@ -49,13 +52,9 @@ public class ShopTest {
 
     @Test
     public void canCalculateTotalMarkup(){
-        shop.addToStock(guitar);
-        shop.addToStock(piano);
-        shop.addToStock(saxophone);
         shop.addToStock(drumsticks);
         shop.addToStock(drumskins);
-        assertEquals(500.00, shop.calculateTotalMarkup(), 0.01);
-
+        assertEquals(1280.99, shop.calculateTotalMarkup(), 0.01);
     }
 
 
